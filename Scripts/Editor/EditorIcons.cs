0d10e8bf62544f6990884890fec6d2f1264ac84c
Herbert Veitengruber <tohklidan@hotmail.com>
2018-12-18 23:26:44 +0100

    added static classes for utils and icons

---
+++ Scripts/Editor/EditorIcons.cs
@@ -0,0 +1,101 @@
+﻿/*  __  __      
+ * /\ \/\ \  __________   
+ * \ \ \_\ \/_______  /\   
+ *  \ \  _  \  ____/ / /   
+ *   \ \_\ \_\ \ \/ / /    
+ *    \/_/\/_/\ \ \/ /     Copyright (c) 2018 Herbert Veitengruber
+ *             \ \  /      https://github.com/HerbertV
+ *              \_\/
+ * -----------------------------------------------------------------------------
+ *  Licensed under the MIT License. 
+ *  See LICENSE file in the project root for full license information.
+ */
+using UnityEditor;
+using UnityEngine;
+
+namespace HVUnity.Core.Editor
+{
+	/// <summary>
+	/// Class EditorIcons
+	/// 
+	/// Shortcuts to access icon resources
+	/// </summary>
+	public static class EditorIcons
+	{
+		/*
+		 * =========================================================================
+		 *  Variables
+		 * =========================================================================
+		 */
+
+		#region csharp script icons
+
+		/// <summary>
+		/// Generic Unity build-in csharp icon
+		/// </summary>
+		private static Texture2D csharpGenericIcon;
+
+
+		private static Texture2D csharpBehaviourIcon;
+
+		private static Texture2D csharpAbtractIcon;
+
+		private static Texture2D csharpExtensionIcon;
+
+		private static Texture2D csharpInterfaceIcon;
+
+		private static Texture2D csharpSingletonIcon;
+
+		#endregion
+
+		#region other icons
+
+		/// <summary>
+		/// Icon for Scriptable settings objects
+		/// </summary>
+		private static Texture2D scriptableSettingsIcon;
+
+		#endregion
+		/*
+		 * =========================================================================
+		 *  Accessors
+		 * =========================================================================
+		 */
+
+		/// <summary>
+		/// Generic Unity build-in csharp icon
+		/// </summary>
+		public static Texture2D CSharpGenericIcon
+		{
+			get
+			{
+				if( csharpGenericIcon == null )
+					csharpGenericIcon = (Texture2D)EditorGUIUtility.IconContent("cs Script Icon").image;
+
+				return csharpGenericIcon;
+			}
+		}
+
+		/// <summary>
+		/// Icon for Scriptable settings objects
+		/// </summary>
+		public static Texture2D ScriptableSettingsIcon
+		{
+			get
+			{
+				if( scriptableSettingsIcon == null )
+				{
+					scriptableSettingsIcon = (Texture2D)AssetDatabase.LoadAssetAtPath(
+							"Assets/TheWorkshop/HVUnityCore/Icons/Editor/ScriptableSettings.png",
+							typeof(Texture2D)
+						);
+
+					// TODO package path
+				}
+				return scriptableSettingsIcon;
+			}
+		}
+
+
+	}
+}
---
+++ Scripts/Editor/EditorUtils.cs
@@ -0,0 +1,53 @@
+﻿/*  __  __      
+ * /\ \/\ \  __________   
+ * \ \ \_\ \/_______  /\   
+ *  \ \  _  \  ____/ / /   
+ *   \ \_\ \_\ \ \/ / /    
+ *    \/_/\/_/\ \ \/ /     Copyright (c) 2018 Herbert Veitengruber
+ *             \ \  /      https://github.com/HerbertV
+ *              \_\/
+ * -----------------------------------------------------------------------------
+ *  Licensed under the MIT License. 
+ *  See LICENSE file in the project root for full license information.
+ */
+using System.IO;
+
+using UnityEditor;
+using UnityEngine;
+
+namespace HVUnity.Core.Editor
+{
+	/// <summary>
+	/// Class EditorUtils
+	/// 
+	/// Collection of useful editor utilities.
+	/// </summary>
+	public static class EditorUtils
+	{
+		/// <summary>
+		/// returns the current path of a selected project folder or "Assets"
+		/// </summary>
+		/// <returns></returns>
+		public static string getSelectedProjectWindowPath()
+		{
+			Object obj = Selection.activeObject;
+
+			if( obj == null )
+				return "Assets";
+
+			string path = AssetDatabase.GetAssetPath(obj);
+			
+			if( !string.IsNullOrEmpty(path) )
+			{
+				if( File.Exists(path) )
+					return Path.GetDirectoryName(path);
+				else
+					return path;
+			}
+			// return default
+			return "Assets";
+		}
+
+		
+	}
+}
